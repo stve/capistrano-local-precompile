c98606adc2554258959669ec2ed5a21d7eef3c36
Tom Caflisch <tomcaflisch@gmail.com>
2017-06-07 20:46:00 -0500

    Trying to fix bug

--- lib/capistrano/local_precompile.rb
+++ lib/capistrano/local_precompile.rb
@@ -38,8 +38,8 @@ namespace :deploy do
     task :prepare do
       run_locally do
         with rails_env: fetch(:precompile_env) do
-          execute :rake, "assets:clean"
-          execute :bundle, "exec rake assets:precompile"
+          execute "rake assets:clean"
+          execute "rake assets:precompile"
         end
       end
     end
