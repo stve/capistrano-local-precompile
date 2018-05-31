4f2b9c30cc0c1a20a569dd5f7848c47430508203
Tom Caflisch <tomcaflisch@gmail.com>
2018-05-31 13:47:16 -0500

    Cleanup packs
    Cleanup packs_dir after precompile

--- lib/capistrano/local_precompile.rb
+++ lib/capistrano/local_precompile.rb
@@ -32,6 +32,7 @@ namespace :deploy do
       run_locally do
         with rails_env: fetch(:precompile_env) do
           execute "rm -rf", fetch(:assets_dir)
+          execute "rm -rf", fetch(:packs_dir)
         end
       end
     end
