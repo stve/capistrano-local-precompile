91fb17f65921d472397e14e7a59d8da9e9335bd3
Sergey Malykh <xronos.i.am@gmail.com>
2019-02-04 19:10:41 +0300

    Cherry pick some fixes from forks (#29)
    * Use parallel execution for rsync
    
    * added ssh port to the rsync command
    
    * fix fetching precompile_env
    
    * fix ssh port

--- lib/capistrano/local_precompile.rb
+++ lib/capistrano/local_precompile.rb
@@ -1,6 +1,5 @@
 namespace :load do
   task :defaults do
-    set :precompile_env,   fetch(:rails_env) || 'production'
     set :assets_dir,       "public/assets"
     set :packs_dir,        "public/packs"
     set :rsync_cmd,        "rsync -av --delete"
@@ -25,7 +24,8 @@ namespace :deploy do
     desc "Actually precompile the assets locally"
     task :prepare do
       run_locally do
-        with rails_env: fetch(:precompile_env) do
+        precompile_env = fetch(:precompile_env) || fetch(:rails_env) || 'production'
+        with rails_env: precompile_env do
           execute "rake", "assets:clean"
           execute "rake", "assets:precompile"
         end
@@ -34,10 +34,11 @@ namespace :deploy do
 
     desc "Performs rsync to app servers"
     task :rsync do
-      on roles(fetch(:assets_role)) do |server|
+      on roles(fetch(:assets_role)), in: :parallel do |server|
         run_locally do
-          execute "#{fetch(:rsync_cmd)} ./#{fetch(:assets_dir)}/ #{server.user}@#{server.hostname}:#{release_path}/#{fetch(:assets_dir)}/" if Dir.exists?(fetch(:assets_dir))
-          execute "#{fetch(:rsync_cmd)} ./#{fetch(:packs_dir)}/ #{server.user}@#{server.hostname}:#{release_path}/#{fetch(:packs_dir)}/" if Dir.exists?(fetch(:packs_dir))
+          remote_shell = %(-e "ssh -p #{server.port}") if server.port
+          execute "#{fetch(:rsync_cmd)} #{remote_shell} ./#{fetch(:assets_dir)}/ #{server.user}@#{server.hostname}:#{release_path}/#{fetch(:assets_dir)}/" if Dir.exists?(fetch(:assets_dir))
+          execute "#{fetch(:rsync_cmd)} #{remote_shell} ./#{fetch(:packs_dir)}/ #{server.user}@#{server.hostname}:#{release_path}/#{fetch(:packs_dir)}/" if Dir.exists?(fetch(:packs_dir))
         end
       end
     end
